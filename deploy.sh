b17c1467a289c189acd054eaa253d2cca858fa10
LuH4wk <luyandah@explore-datascience.net>
2023-02-03 12:08:45 +0200

    change deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 #git push -f git@github.com:Th3Gu7u88/Th3Gu7u88.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Th3Gu7u88/Th3Gu7u88.git main:develop
+git push -f git@github.com:Th3Gu7u88/Th3Gu7u88.git main:gh-pages
 
 cd -
